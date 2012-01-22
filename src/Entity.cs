eab25ab79aa1d97248e6291e82a6c4ab3860e840
Andy Nygard <andreas.nygard@gmail.com>
2012-01-22 13:18:51 +1100

    Created project with IComponent and Entity classes.

---
+++ src/Entity.cs
@@ -0,0 +1,26 @@
+﻿namespace EntitySystem
+{
+    /// <summary>
+    /// An entity in the Entity System.
+    /// <para />
+    /// An entity represents one game object in the game world. The aspects (or 'behaviour') of the entity is decorated
+    /// via the EntityManager. The entity is a loose representation of the game object, as the Entity class itself
+    /// contains no data or methods - it is simply a label.
+    /// </summary>
+    public sealed class Entity
+    {
+        /// <summary>
+        /// Initializes a new instance of the Entity class.
+        /// </summary>
+        /// <param name="id">The unique id of this entity.</param>
+        public Entity(int id)
+        {
+            this.Id = id;
+        }
+
+        /// <summary>
+        /// Gets the entity's unique id.
+        /// </summary>
+        public int Id { get; private set; }
+    }
+}
---
+++ src/IComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace EntitySystem
+{
+    /// <summary>
+    /// A component in the Entity system.
+    /// <para />
+    /// A component represents one aspect of an entity in the game world. It may only contain data members and cannot
+    /// overlap the responsibility of another component (eg. there can only be one component which maintain's an
+    /// entity's x/y position.
+    /// </summary>
+    public interface IComponent
+    {
+    }
+}
