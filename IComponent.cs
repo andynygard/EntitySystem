d4a6df92766673659e23057e119891383a1fd6fe
Andy Nygard <andreas.nygard@gmail.com>
2012-01-22 18:11:18 +1100

    Fixed typo in comment.

--- IComponent.cs
+++ IComponent.cs
@@ -5,7 +5,7 @@
     /// <para />
     /// A component represents one aspect of an entity in the game world. It may only contain data members and cannot
     /// overlap the responsibility of another component (eg. there can only be one component which maintain's an
-    /// entity's x/y position.
+    /// entity's x/y position).
     /// </summary>
     public interface IComponent
     {
