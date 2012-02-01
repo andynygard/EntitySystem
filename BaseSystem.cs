aae15b51c1de8f7bb2d195881cd5aa0b79742aff
Andy Nygard <andreas.nygard@gmail.com>
2012-02-01 20:17:41 +1100

    Added base implementation of ISystem

---
+++ BaseSystem.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="BaseSystem.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace EntitySystem
+{
+    /// <summary>
+    /// A base abstract implementation of the ISystem interface.
+    /// </summary>
+    public abstract class BaseSystem : ISystem
+    {
+        /// <summary>
+        /// Initializes a new instance of the BaseSystem class.
+        /// </summary>
+        /// <param name="entityManager">The EntityManager for the world that this system belongs to.</param>
+        public BaseSystem(EntityManager entityManager)
+        {
+            this.Enabled = true;
+            this.EntityManager = entityManager;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this system should be processed.
+        /// </summary>
+        public bool Enabled { get; private set; }
+
+        /// <summary>
+        /// Gets the EntityManager for the world that this system belongs to.
+        /// </summary>
+        protected EntityManager EntityManager { get; private set; }
+
+        /// <summary>
+        /// Perform the system's processing.
+        /// </summary>
+        /// <param name="delta">The number of milliseconds since the last processing occurred.</param>
+        public abstract void Process(int delta);
+    }
+}
