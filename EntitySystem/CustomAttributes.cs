fb240c154d5c618f09af89f6d8363d53788162f0
Andreas Nygard <andreas.nygard@gmail.com>
2012-03-22 15:36:00 +1100

    Added the ESIgnoreData attribute to skip serializing some component properties.

---
+++ EntitySystem/CustomAttributes.cs
@@ -0,0 +1,12 @@
+﻿namespace EntitySystem
+{
+    using System;
+
+    /// <summary>
+    /// Indicates that this property should be ignored when serializing the Entity System.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ESIgnoreData : Attribute
+    {
+    }
+}
